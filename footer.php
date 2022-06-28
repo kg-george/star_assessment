7f00c0d0eddf75ec4122acb62b7c1d028ecec386
kg-george <108368473+kg-george@users.noreply.github.com>
2022-06-28 23:24:55 +0300

    Uploading footer.php

---
+++ footer.php
@@ -0,0 +1,15 @@
+<link
+  href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/4.2.0/mdb.min.css"
+  rel="stylesheet"
+/>
+
+<footer style="background-color:#121015;position:static;bottom:0px;width:100%;margin-top:15px;">
+  
+      <h4 style="text-align:center;padding-top:5px;">Star.GR</h4>
+
+
+  <div class="text-center p-4" >
+    © 2022 Copyright:
+    <span class="text-reset fw-bold">Kostagiannis Giorgos </span>
+  </div>
+</footer>
